ed99d05665ac6645a22b1588040f67aca79d66ca
root <root@localhost.localdomain>
2016-04-20 12:34:23 +0200

    upload application

---
+++ app/Http/routes.php
@@ -0,0 +1,102 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| This route group applies the "web" middleware group to every route
+| it contains. The "web" middleware group is defined in your HTTP
+| kernel and includes session state, CSRF protection, and more.
+|
+*/
+
+use App\Task;
+use Illuminate\Http\Request;
+
+Route::group(['middleware' => ['web']], function () {
+    /**
+     * Show Task Dashboard
+     */
+
+   // });
+
+
+Route::get('/', function () {
+        return view('welcome');
+    });
+
+Route::get('home2', function () {
+if(Auth::guest()){
+return Redirect::to('auth/login');
+}else{
+echo 'Your Are Auth user' . Auth::user()->email . '.' ;
+}   
+ });
+
+
+//Route::get('task', function () {
+//if(Auth::guest()){
+//return Redirect::to('auth/login');
+//}else{
+  //      return view('view');
+//}
+   // });
+
+
+//Authenication Rules ... AuthController
+Route::get('auth/login','Auth\AuthController@getLogin');
+Route::post('auth/login','Auth\AuthController@postLogin');
+Route::get('auth/logout','Auth\AuthController@logout');
+//Registeration Routes 
+Route::get('auth/register','Auth\AuthController@getRegister');
+Route::post('auth/register','Auth\AuthController@postRegister');
+
+    Route::get('/view', function () {
+if(Auth::guest()){
+return Redirect::to('auth/login');
+}else{
+
+        return view('tasks', [
+            'tasks' => Task::orderBy('created_at', 'desc')->get()
+        ]);
+}
+    });
+
+
+    /**
+     * Add New Task
+     */
+    Route::post('/task', function (Request $request) {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|max:255',
+	    'owner' => 'required|max:255',
+        ]);
+
+        if ($validator->fails()) {
+            return redirect('/view')
+                ->withInput()
+                ->withErrors($validator);
+        }
+
+        $task = new Task;
+        $task->name = $request->name;
+        $task->owner = $request->owner;
+        $task->save();
+
+        return redirect('/view');
+    });
+
+    /**
+     * Delete Task
+     */
+    Route::delete('/task/{id}', function ($id) {
+        Task::findOrFail($id)->delete();
+
+        return redirect('/view');
+    });
+});
+
+Route::auth();
+
+Route::get('/home', 'HomeController@index');
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.app')
+
+@section('content')
+
+<style>
+ .container {
+                text-align: center;
+                display: table-cell;
+                vertical-align: middle;
+            }
+
+            .title {
+                font-size: 96px;
+font-family: 'Lato';
+
+            }
+
+ .subtitle {
+                font-size: 32px;
+                color:darkred;
+font-family: 'Lato';
+ 
+           }
+        </style>
+
+<div class="container">
+            <div class="content">
+                <div class="title"> Laravel 5 </div>
+                <div class="subtitle">... Just Learning How To Code ... </div>
+            </div>
+        </div>
+
+
+@endsection
